578034c91d0bcdb458a6c1adb21a3bbd235e7593
Toko10 <tornike.pertsuliani69@gmail.com>
2025-12-25 18:09:07 +0400

    Add files via upload

---
+++ TornikePertsulianiQuiz.py
@@ -0,0 +1,70 @@
+# #N1
+#
+# random_list = ["xinkali", "jasurbeki", "mwvadi", "LarianiMoxito"]
+# def string_len(strings):
+#     lengths = []
+#     for random_list in strings:
+#         lengths.append(len(random_list))
+#     return lengths
+# print(string_len(random_list))
+#
+#N2
+#
+# list1 = [1, 2, 3, 4]
+# list2 = [4, 5, 6, 7]
+# def common_unique_elements(list1, list2):
+#     list3 = set(list1) ^ (set(list2))
+#     return list3
+# print(common_unique_elements(list1, list2))
+#
+#N3
+#
+# people = [("ჯასურ", 90), ("ბეკი", 25)]
+# def duysdgibuasdbu(data):
+#     result = {}
+#     for name, age in data:
+#         result[name] = age
+#     return result
+# result_dict = duysdgibuasdbu(people)
+# for name, age in result_dict.items():
+#     print(name, age)
+#
+#
+#
+#N5
+#
+# list1 = ["abanotubani", "antalia", "borshi", "choxatauri", "ananuri", "ჭia"]
+# def shubgfdbuy(words):
+#     result = {}
+#     for s in words:
+#         if s[0] == "a":
+#             result[s] = len(s)
+#     return result
+# print(shubgfdbuy(list1))
+#
+#N6
+#
+# dicard, remove, pop
+#
+#N7
+#
+# tuple aris ucvladi da listshi chamateba da amogheba sheidzleba
+#
+#N8
+#
+# union, intersection, difference, symmetric_difference
+#
+#N9
+#
+# random_dict={
+#     "iago": 41
+# }
+# random_dict["jasurbeki"] = 67
+#
+#N10
+#
+#print(random_dict)
+#
+#N11
+#
+#def random_def(*N)
